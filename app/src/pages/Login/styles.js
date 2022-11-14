db9bfe7928690114c7a868260dad1675d899a7c2
JottaEnnie <jncontatojn@gmail.com>
2022-11-14 16:14:35 -0300

    backgroudColor #000

---
+++ app/src/pages/Login/styles.js
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native'
+
+const styles = StyleSheet.create({
+    bgDark: {
+        backgroundColor: '#000',
+        flex: 1,
+    }
+
+})
+
+export default styles;
