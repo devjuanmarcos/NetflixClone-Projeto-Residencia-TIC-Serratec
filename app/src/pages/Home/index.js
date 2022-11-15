a266655eacb8108eafd438329aa7832e973c1932
JottaEnnie <jncontatojn@gmail.com>
2022-11-15 17:10:06 -0300

    previa funcionando

--- app/src/pages/Home/index.js
+++ app/src/pages/Home/index.js
@@ -1,32 +1,53 @@
 import React from "react";
-import { Text, View, TouchableOpacity } from "react-native";
+import { View, ScrollView, TouchableOpacity, FlatList, Image } from "react-native";
 
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import styles from './styles';
-import { Button } from 'react-native-paper';
+import { Button, Title } from 'react-native-paper';
 
 import Header from '../../components/Header';
 import Hero from '../../components/Hero';
 import ButtonVertical from "../../components/ButtonVertical";
-
+import LinearGradient from "react-native-linear-gradient";
 
 const Home = () => {
     return (
-        <View style={styles.container}>
+        <ScrollView style={styles.container}>
             <Header />
             <Hero />
 
             <View style={styles.menuHeader}>
-                <ButtonVertical icon="plus" text="Minha Lista"/>
+                <ButtonVertical icon="plus" text="Minha Lista" />
                 <Button icon="play" uppercase={false} mode="contained" color="#fff">
                     Assitir
                 </Button>
-                <ButtonVertical icon="information-outline" text="Saiba Mais"/>
-
-                
+                <ButtonVertical icon="information-outline" text="Saiba Mais" />
             </View>
 
-        </View>
+            <View style={styles.previaContainer}>
+                <Title style={styles.previaTittle}>Prévias</Title>
+                <FlatList
+                    horizontal
+                    style={styles.flatListContainer}
+                    data={[1, 2, 3, 4, 5, 6, 7]}
+                    renderItem={(item, index) => (
+                        <TouchableOpacity key={index}
+                            style={{ marginLeft: index === 1 ? 20 : 0, marginLeft: 10 }}>
+                            <View style={styles.oval}>
+                                <Image style={styles.capa} source={{ uri: 'https://i.imgur.com/EJyDFeY.jpg' }} />
+
+                                <Image
+                                    resizeMode="contain"
+                                    style={styles.logo}
+                                    source={{ uri: 'https://i.imgur.com/4xN7wB8.png' }} />
+
+                                <LinearGradient style={styles.gradient} colors={['rgba(0,0,0,0)', 'rgba(0,0,0,1)']} />
+
+                            </View>
+                        </TouchableOpacity>
+                    )}
+                />
+            </View>
+        </ScrollView>
 
 
     );
--- app/src/pages/Home/styles.js
+++ app/src/pages/Home/styles.js
@@ -11,7 +11,49 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-evenly'
     },
-   
+    flatListContainer: {
+        width: '100%',
+        height: 100,
+        marginTop: 10,
+        marginBottom: 50,
+    },
+    oval: {
+        backgroundColor: '#E50914',
+        padding: 2.5,
+        width: 90,
+        height: 90,
+        borderRadius: 90,
+    },
+    capa: {
+        height: 85,
+        width: 85,
+        borderRadius: 85
+    },
+    logo: {
+        width: 90,
+        height: 45,
+        position: 'absolute',
+        zIndex: 10,
+        bottom: 10,
+        alignSelf: 'center'
+    },
+
+    previaContainer: {
+        width: '100%',
+        marginTop: 50
+    },
+    previaTittle: {
+        marginLeft: 20
+    },
+    gradient: {
+        width: '100%',
+        height: 60,
+        position: 'absolute',
+        zIndex: 9,
+        bottom: 0 
+
+    }
+
 })
 
 export default styles;
