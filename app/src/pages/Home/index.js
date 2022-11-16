54168ca4707c92b1d95a9b3c18edc3298dfae1b0
JottaEnnie <jncontatojn@gmail.com>
2022-11-16 18:03:16 -0300

    componentizando Previas

--- app/src/pages/Home/index.js
+++ app/src/pages/Home/index.js
@@ -9,6 +9,8 @@ import Hero from '../../components/Hero';
 import ButtonVertical from "../../components/ButtonVertical";
 import LinearGradient from "react-native-linear-gradient";
 
+import Previas from '../../components/Previas'
+
 const Home = () => {
     return (
         <ScrollView style={styles.container}>
@@ -25,27 +27,7 @@ const Home = () => {
 
             <View style={styles.previaContainer}>
                 <Title style={styles.previaTittle}>Prévias</Title>
-                <FlatList
-                    horizontal
-                    style={styles.flatListContainer}
-                    data={[1, 2, 3, 4, 5, 6, 7]}
-                    renderItem={(item, index) => (
-                        <TouchableOpacity key={index}
-                            style={{ marginLeft: index === 1 ? 20 : 0, marginLeft: 10 }}>
-                            <View style={styles.oval}>
-                                <Image style={styles.capa} source={{ uri: 'https://i.imgur.com/EJyDFeY.jpg' }} />
-
-                                <Image
-                                    resizeMode="contain"
-                                    style={styles.logo}
-                                    source={{ uri: 'https://i.imgur.com/4xN7wB8.png' }} />
-
-                                <LinearGradient style={styles.gradient} colors={['rgba(0,0,0,0)', 'rgba(0,0,0,1)']} />
-
-                            </View>
-                        </TouchableOpacity>
-                    )}
-                />
+               <Previas/>
             </View>
         </ScrollView>
 
--- app/src/pages/Home/styles.js
+++ app/src/pages/Home/styles.js
@@ -17,26 +17,6 @@ const styles = StyleSheet.create({
         marginTop: 10,
         marginBottom: 50,
     },
-    oval: {
-        backgroundColor: '#E50914',
-        padding: 2.5,
-        width: 90,
-        height: 90,
-        borderRadius: 90,
-    },
-    capa: {
-        height: 85,
-        width: 85,
-        borderRadius: 85
-    },
-    logo: {
-        width: 90,
-        height: 45,
-        position: 'absolute',
-        zIndex: 10,
-        bottom: 10,
-        alignSelf: 'center'
-    },
 
     previaContainer: {
         width: '100%',
@@ -45,14 +25,7 @@ const styles = StyleSheet.create({
     previaTittle: {
         marginLeft: 20
     },
-    gradient: {
-        width: '100%',
-        height: 60,
-        position: 'absolute',
-        zIndex: 9,
-        bottom: 0 
 
-    }
 
 })
 
