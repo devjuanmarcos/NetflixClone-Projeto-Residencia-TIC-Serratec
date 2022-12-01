c7bb3220997c1e6e9534f7889c5608038d10c85f
Juan Marcos de Souza Texe <111091371+devjuanmarcos@users.noreply.github.com>
2022-12-01 11:30:06 -0300

    rotas REST com express

--- ws/index.js
+++ ws/index.js
@@ -4,9 +4,33 @@ const app = express();
 
 app.use(morgan("dev"));
 
+// recuperar todos os registros
 app.get("/", (req, res) => {
-  // regra de negócio
-  res.json({ mensagem: "Rota funcionando" });
+  res.json({ mensagem: "PEGAR TODOS OS REGISTROS" });
+});
+
+// pegar registro somente com o id
+app.get("/:id", (req, res) => {
+  const id = req.params.id;
+  res.json({ mensagem: `PEGAR SOMENTE O REGISTRO COM O ID: ${id}` });
+});
+
+// criar um registro
+app.post("/", (req, res) => {
+  const body = req.body;
+  res.json(body);
+});
+
+// atualizar registro somente com o id
+app.put("/:id", (req, res) => {
+  const id = req.params.id;
+  res.json({ mensagem: `ATUALIZAR SOMENTE O REGISTRO COM O ID: ${id}` });
+});
+
+// deletar registro somente com o id
+app.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  res.json({ mensagem: `DELETAR SOMENTE O REGISTRO COM O ID: ${id}` });
 });
 
 app.listen(3000, () => {
