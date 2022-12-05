0690e22748d3bdede29cd7ff0f18babdb5162cbb
Juan Marcos de Souza Texe <111091371+devjuanmarcos@users.noreply.github.com>
2022-12-05 11:07:48 -0300

    conectando ao mongoDb com mongoose

--- ws/index.js
+++ ws/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const app = express();
 
 const routes = require("./src/routes/filmes.routes");
+mongoose.connect("mongodb://localhost:27017/netflix");
 
 app.use(bodyParser.json());
 app.use(morgan("dev"));
---
+++ ws/src/routes/filmes.routes.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+
+// recuperar todos os registros
+router.get("/", (req, res) => {
+  res.json({ mensagem: "PEGAR TODOS OS REGISTROS" });
+});
+
+// pegar registro somente com o id
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  res.json({ mensagem: `PEGAR SOMENTE O REGISTRO COM O ID: ${id}` });
+});
+
+// criar um registro
+router.post("/", (req, res) => {
+  const body = req.body;
+  res.json(body);
+});
+
+// atualizar registro somente com o id
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+  res.json({ mensagem: `ATUALIZAR SOMENTE O REGISTRO COM O ID: ${id}` });
+});
+
+// deletar registro somente com o id
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  res.json({ mensagem: `DELETAR SOMENTE O REGISTRO COM O ID: ${id}` });
+});
+
+module.exports = router;
