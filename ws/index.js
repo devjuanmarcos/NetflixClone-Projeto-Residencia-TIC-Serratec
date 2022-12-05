2e859a12c577a48f30ee67721660ee27d8307878
Juan Marcos de Souza Texe <111091371+devjuanmarcos@users.noreply.github.com>
2022-12-05 10:58:37 -0300

    estruturando rotas externas

--- ws/index.js
+++ ws/index.js
@@ -3,37 +3,11 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const app = express();
 
+const routes = require("./src/routes/filmes.routes");
+
 app.use(bodyParser.json());
 app.use(morgan("dev"));
-
-// recuperar todos os registros
-app.get("/", (req, res) => {
-  res.json({ mensagem: "PEGAR TODOS OS REGISTROS" });
-});
-
-// pegar registro somente com o id
-app.get("/:id", (req, res) => {
-  const id = req.params.id;
-  res.json({ mensagem: `PEGAR SOMENTE O REGISTRO COM O ID: ${id}` });
-});
-
-// criar um registro
-app.post("/", (req, res) => {
-  const body = req.body;
-  res.json(body);
-});
-
-// atualizar registro somente com o id
-app.put("/:id", (req, res) => {
-  const id = req.params.id;
-  res.json({ mensagem: `ATUALIZAR SOMENTE O REGISTRO COM O ID: ${id}` });
-});
-
-// deletar registro somente com o id
-app.delete("/:id", (req, res) => {
-  const id = req.params.id;
-  res.json({ mensagem: `DELETAR SOMENTE O REGISTRO COM O ID: ${id}` });
-});
+app.use("/", routes);
 
 app.listen(3000, () => {
   console.log("Meu servidor está funcionando");
