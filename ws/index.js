0bcb2be12c129a14ed7ad0662ff10df9132866b0
Juan Marcos de Souza Texe <111091371+devjuanmarcos@users.noreply.github.com>
2022-12-07 10:11:45 -0300

    construindo rota de login

--- ws/index.js
+++ ws/index.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
-const app = express();
 const database = require("./src/services/database");
-const routes = require("./src/routes/filmes.routes");
+const cors = require("cors");
+const app = express();
 
+const filmeRoutes = require("./src/routes/filmes.routes");
+const usuarioRoutes = require("./src/routes/usuarios.routes");
+
+/// MIDDLEWARES
 app.use(bodyParser.json());
+app.use(cors());
 app.use(morgan("dev"));
-app.use("/", routes);
+
+/// ROUTES
+app.use("/", filmeRoutes);
+app.use("/usuario", usuarioRoutes);
 
 app.listen(3000, () => {
   console.log("Meu servidor está funcionando");
