e39e77e650019d23c5409349dab2b297b5b03479
Juan Marcos de Souza Texe <111091371+devjuanmarcos@users.noreply.github.com>
2022-12-08 15:46:32 -0300

    modal de opcoes parte 1

--- app/src/pages/Filme/index.js
+++ app/src/pages/Filme/index.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-
+import {SinglePickerMaterialDialog} from 'react-native-material-dialog';
 import {Button, Title, Paragraph, Caption} from 'react-native-paper';
 import {
   ScrollView,
@@ -19,58 +19,78 @@ import Episodio from '../../components/Episodio';
 
 const Filme = () => {
   const [tipo] = useState('serie');
+  const [visible, setVisible] = useState(false);
+  const [temporada, setTemporada] = useState({value: 1, label: 'Temporada 1'});
 
   return (
-    <ScrollView style={styles.container}>
-      <ImageBackground
-        source={{uri: 'https://i.imgur.com/EJyDFeY.jpg'}}
-        style={styles.hero}
+    <>
+      <SinglePickerMaterialDialog
+        title={'Séris - Temporadas'}
+        items={[
+          {value: 1, label: 'Temporada 1'},
+          {value: 2, label: 'Temporada 2'},
+          {value: 3, label: 'Temporada 3'},
+        ]}
+        visible={visible}
+        selectedItem={temporada}
+        onOk={result => {
+          setVisible(false);
+          setTemporada(result.selectedItem);
+        }}
       />
-      <View style={styles.containerPadding}>
-        <Title>Nome do Filme</Title>
-        <Button
-          style={styles.buttonPlay}
-          icon="play"
-          uppercase={false}
-          mode="contained"
-          color="#fff">
-          Assistir
-        </Button>
-        <Paragraph>
-          Texto totalmente aleatório apenas para encher linguiça no meu
-          parágrafo
-        </Paragraph>
-        <Caption style={styles.captionInfos}>
-          Elenco:{' '}
-          <Caption style={styles.captionWhite}>
-            Juan Marcos, Juan Marcos, Juan Marcos e Juan{' '}
-          </Caption>{' '}
-          Gêneros:{' '}
-          <Caption style={styles.captionWhite}>
-            Ação, muita Aventura e Suspense pra caramba
-          </Caption>{' '}
-        </Caption>
-        <View style={styles.menu}>
-          <ButtonVertical icon="plus" text="Minha Lista" />
-          <ButtonVertical icon="thumb-up" text="Classifique" />
-          <ButtonVertical icon="send" text="Compartilhe" />
-          <ButtonVertical icon="download" text="Baixar" />
+      <ScrollView style={styles.container}>
+        <ImageBackground
+          source={{uri: 'https://i.imgur.com/EJyDFeY.jpg'}}
+          style={styles.hero}
+        />
+        <View style={styles.containerPadding}>
+          <Title>Nome do Filme</Title>
+          <Button
+            style={styles.buttonPlay}
+            icon="play"
+            uppercase={false}
+            mode="contained"
+            color="#fff">
+            Assistir
+          </Button>
+          <Paragraph>
+            Texto totalmente aleatório apenas para encher linguiça no meu
+            parágrafo
+          </Paragraph>
+          <Caption style={styles.captionInfos}>
+            Elenco:{' '}
+            <Caption style={styles.captionWhite}>
+              Juan Marcos, Juan Marcos, Juan Marcos e Juan{' '}
+            </Caption>{' '}
+            Gêneros:{' '}
+            <Caption style={styles.captionWhite}>
+              Ação, muita Aventura e Suspense pra caramba
+            </Caption>{' '}
+          </Caption>
+          <View style={styles.menu}>
+            <ButtonVertical icon="plus" text="Minha Lista" />
+            <ButtonVertical icon="thumb-up" text="Classifique" />
+            <ButtonVertical icon="send" text="Compartilhe" />
+            <ButtonVertical icon="download" text="Baixar" />
+          </View>
+          {tipo === 'serie' && (
+            <>
+              <TouchableOpacity
+                onPress={() => setVisible(true)}
+                style={styles.buttonTemporada}>
+                <Text style={styles.temporadaName}>Temporada 1</Text>
+                <Icon name="chevron-down" color="#fff" size={20} />
+              </TouchableOpacity>
+              <FlatList
+                data={[1, 2, 3, 4]}
+                renderItem={({item, index}) => <Episodio key={index} />}
+              />
+            </>
+          )}
         </View>
-        {tipo === 'serie' && (
-          <>
-            <TouchableOpacity style={styles.buttonTemporada}>
-              <Text style={styles.temporadaName}>Temporada 1</Text>
-              <Icon name="chevron-down" color="#fff" size={20} />
-            </TouchableOpacity>
-            <FlatList
-              data={[1, 2, 3, 4]}
-              renderItem={({item, index}) => <Episodio key={index} />}
-            />
-          </>
-        )}
-      </View>
-      {tipo === 'filme' && <Secao hasTopBorder />}
-    </ScrollView>
+        {tipo === 'filme' && <Secao hasTopBorder />}
+      </ScrollView>
+    </>
   );
 };
 
